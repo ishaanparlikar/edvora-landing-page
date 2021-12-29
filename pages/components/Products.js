b832c546d32f79789eddc16c97325b48f9f46417
Ishan Parlikar <ishan@shadesofweb.com>
2021-12-30 01:33:58 +0530

    Added Title

--- pages/components/Products.js
+++ pages/components/Products.js
@@ -15,6 +15,8 @@ const Products = () => {
    //   console.log(products);
    return (
       <>
+         <h1 className="text-3xl text-white font-bold mb-4">Edvora</h1>
+         <h2 className="text-xl text-gray-300 mb-4">Products</h2>
          <Slider data={arrayOfObj} />
 
       </>
